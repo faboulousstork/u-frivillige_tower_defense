a10515fe18298ba2b4352259541ce6aaceb8f486
faboulousstork <159310520+faboulousstork@users.noreply.github.com>
2025-04-23 17:03:46 +0200

    alt  nogensinde ever altid

--- ufrivilligt_tower_defense/Assets/Rasmus_creator.cs
+++ ufrivilligt_tower_defense/Assets/Rasmus_creator.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Rasmus_creator : MonoBehaviour
 {
+    public Color green = Color.green;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpriteRenderer SpriteRenderer = GetComponent<SpriteRenderer>();
     }
-
+  
     // Update is called once per frame
     void Update()
     {
-        
     }
-}
+    
+    void OnMouseOver() {
+        Debug.Log("dint");
+     if (Input.GetKeyUp(KeyCode.Mouse0)){
+            Debug.Log("clickity");
+            SpriteRenderer.color = green;
+     }
+        }
+        }
+
